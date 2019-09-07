f4a4d117029b10a6d2605996f73f814cb8dfdb7e
Francisco Gonzalez <francisco@primestaram.com>
2019-09-07 13:55:32 -0400

    front end routes complete

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,10 +1,16 @@
+const path = require("path");
+
 module.exports={
+    displayHome:function(req,res)
+    {
+        res.sendFile(path.join(__dirname, "home.html"))
+    },
     displayTables:function(req,res)
     {
-
+        res.sendFile(path.join(__dirname, "tables.html"))
     },
     displayReservationForm:function(req,res)
     {
-
+        res.sendFile(path.join(__dirname, "reservation.html"))
     }
 }
